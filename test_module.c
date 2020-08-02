06b1614b5e273fc98f518fce86bab9dd2cfe7ae8
ThatWDHGuy <will@oceanplait.com>
2020-08-02 22:41:29 +1200

    Changed print, still getting the hang of cli git

--- test_module.c
+++ test_module.c
@@ -2,12 +2,12 @@
 #include <linux/module.h>
 
 int test_module_init(void){
-	printk(KERN_ALERT "Inside the %s function\n", __FUNCTION__);
+	printk(KERN_ALERT "Inside the %s function, module added\n", __FUNCTION__);
 	return 0;
 }
 
 void test_module_exit(void){
-	printk(KERN_ALERT "Inside the %s function\n", __FUNCTION__);
+	printk(KERN_ALERT "Inside the %s function, removing module\n", __FUNCTION__);
 }
 
 module_init(test_module_init);
